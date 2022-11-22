5ca8bbe44b7ca0be90673f4939a216ab635c6b7e
Andzej-F <70884391+Andzej-F@users.noreply.github.com>
2022-11-22 11:54:48 +0200

    Initial commit

---
+++ src/main/java/_Chapter_8_More_OOP/Rank.java
@@ -0,0 +1,17 @@
+package _Chapter_8_More_OOP;
+
+public enum Rank {
+  ACE,
+  TWO,
+  THREE,
+  FOUR,
+  FIVE,
+  SIX,
+  SEVEN,
+  EIGHT,
+  NINE,
+  TEN,
+  JACK,
+  QUEEN,
+  KING;
+}
---
+++ src/main/java/_Chapter_8_More_OOP/Suit.java
@@ -0,0 +1,8 @@
+package _Chapter_8_More_OOP;
+
+public enum Suit {
+  CLUBS,
+  DIAMONDS,
+  HEARTS,
+  SPADES;
+}
